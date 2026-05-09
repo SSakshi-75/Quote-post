87a1c7dfeac3756897d3990991bd37d1120d45c9
SSakshi-75 <sakshisingh092003@gmail.com>
2026-05-09 22:19:44 +0530

    error

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use(cors({
       "https://quote-post-c6lj.vercel.app",
       "https://quote-post.vercel.app",
       "http://localhost:5173",
+      "http://127.0.0.1:5173",
       "http://localhost:3000"
     ];
     if (!origin || allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")) {
